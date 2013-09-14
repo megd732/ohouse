87873fa86c565e1dc16426197ded3817ce59e071
Megan Davis <megan.davis@oberlin.edu>
2013-09-14 13:13:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ohouse::Application.config.secret_token = '5fff8c125acfa9ec2e5ecd580422a59505380b88ffd4de82a0a69f4d895ea8d18495c2b436f280b21feec84ae4d3e32b946faa29895e826a12f6c14a985ba123'
