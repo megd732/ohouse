b6f0d5777622959e7544a900717da019065fe88b
Megan Davis <megan.davis@oberlin.edu>
2013-10-10 23:27:24 -0400

    Add SSL in production

--- app/models/residence.rb
+++
@@ -1,4 +0,0 @@
-class Residence < ActiveRecord::Base
-  attr_accessible :apt_number, :description, :full_bed, :laundry, :notes, :number, :number_bedrooms, :street_name, :twin_bed
-  # add: downstairs, upstairs
-end
