623f3a5710d16c36f6d98bc35c9637b27087059b
Megan Davis <megan.davis@oberlin.edu>
2013-10-11 12:32:21 -0400

    Fixed session storing; sign in, up, and out are all working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ActiveRecord::Base
   private
 
     def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
+      self.remember_token = SecureRandom.base64.tr("+/", "-_")
     end
 end
